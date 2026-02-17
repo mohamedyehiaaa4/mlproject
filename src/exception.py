ba5c3b70312d78a8dddd11a031999d67eabfaa13
mohamedyehiaaa4 <22p0064@eng.asu.edu.eg>
2026-02-17 22:18:45 +0200

    Added data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,15 +1,19 @@
 import sys
-import logging
+try:
+    from src.logger import logger
+except ModuleNotFoundError:
+    from logger import logger
+
 
 def error_message_details(error, error_detail=sys):
     _, _, exc_tb = error_detail.exc_info()
 
-    # Safe fallback if no active traceback
     if exc_tb is None:
         return f"Error message: [{str(error)}]"
 
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
+
     return (
         f"Error occurred in script: [{file_name}] "
         f"at line number: [{line_number}] "
@@ -27,9 +31,8 @@ class CustomException(Exception):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
     try:
         a = 1 / 0
     except Exception as e:
-        logging.exception("Divide by zero error")
+        logger.exception("Divide by zero error")
         raise CustomException(e, sys) from e
--- src/logger.py
+++ src/logger.py
@@ -2,17 +2,26 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    level=logging.INFO,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH, encoding="utf-8"),
+        logging.StreamHandler()
+    ]
 )
 
+
+logger = logging.getLogger("ml_project")
+
 if __name__ == "__main__":
-    logging.info("Logging has started")
+    logger.info("Logging has started")
+    logger.info(f"Log file: {LOG_FILE_PATH}")
